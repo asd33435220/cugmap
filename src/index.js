59e4484786b512b17ef4db14088f1014aed63965
朱宇宸 <5637496+zhu_yu_chen@user.noreply.gitee.com>
2020-12-17 22:28:11 +0800

    feat: 完成了router结构

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Map/App';
+import Login from './Login/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Switch>
+      <Route path='/login' component={Login} />
+      {/* <Route path='/app' component={SideBar} /> */}
+      <Route path='/' component={App} />
+    </Switch>
+  </Router>
+  ,
   document.getElementById('root')
 );
 
